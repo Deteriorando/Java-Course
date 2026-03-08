42bffab19ff3b9ddba8042088fc6ef983db24d45
Deteriorando <arthurgamer9999yt@gmail.com>
2026-03-08 19:51:51 -0300

    Explicando e implementando hashcode.

--- src/JavaCore/Colecoes/hashcode/EqualsTest.java
+++ src/JavaCore/Colecoes/hashcode/EqualsTest.java
@@ -5,7 +5,11 @@ import JavaCore.Colecoes.equals.exemple.Smartphone;
 
 public class EqualsTest {
     public static void main(String[] args) {
-        // O que é hashcode?
+        /* O que é hashcode?
+         * Hash Code é uma implementação de um método que vai dar códigos determinados a um determinado objeto.
+         * Se o equals ele verifica o conteúdo, o Hash code ele dá um código ou etiqueta, como quiser interpretar.
+         * Há varias formas de implementar um código hash em si, vária de cada necessidade de uso.
+         */
 
         Smartphone s1 = new Smartphone("4ABS", "Motorola");
         Smartphone s2 = new Smartphone("4ABS", "Motorola");
@@ -19,6 +23,5 @@ public class EqualsTest {
 
         // -- Fontes --
         // DevDojo: https://youtu.be/YWtGWgTlPwQ?si=wK8H-HtOX5Fyi22e
-        // DEVMEDIA: https://www.devmedia.com.br/sobrescrevendo-o-metodo-equals-em-java/26484?utm_dev=google_ads_pmax&gad_source=1&gad_campaignid=22326280955&gclid=Cj0KCQiA2bTNBhDjARIsAK89wlFp7mwItZ-5lZ487fTPGKeKdLKdmzb3KJ70OCwS578M3upqblYnv1AaAoquEALw_wcB
     }
 }
--- src/JavaCore/Colecoes/hashcode/exemple/Celular.java
+++ src/JavaCore/Colecoes/hashcode/exemple/Celular.java
@@ -12,6 +12,11 @@ public class Celular {
     }
 
     // Como implementar o hashcode na nossa classe?
+    /*
+    * 1 - Se um objetoA.equal(objetoB) for verdadeiro, então o hashcode de ambos têm que ser igual.
+    * 2 - Se dois objetos tem o mesmo hashcode, não necessariamente o equals de um deles tem que ser verdadeiro.
+    * 3 - Se dois objetos forem diferentes, o equals deles tem que ser falso, o JAVA tem que ter certeza que eles são objetos diferentes.
+    * */
 
     @Override
     public boolean equals(Object obj) {
@@ -22,9 +27,10 @@ public class Celular {
         return serialNumber != null && serialNumber.equals(celular.serialNumber);
     }
 
+    // Uma das formas de implementação mais simples.
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
     }
 
     public String getSerialNumber() {
