c3496a34e59a1fd2c33e979dc67fa15cde52895e
Deteriorando <arthurgamer9999yt@gmail.com>
2026-03-08 15:33:38 -0300

    .

--- src/JavaCore/ClassesUtilitarias/Strings/StringBuilder/StringBuilderView.java
+++ src/JavaCore/ClassesUtilitarias/Strings/StringBuilder/StringBuilderView.java
@@ -4,7 +4,7 @@ public class StringBuilderView {
     public static void main(String[] args) {
         //É uma classe focada em otimização.
         //Por tanto, focada nisso, ela é mais complexa que a String.
-
+        //Mais rápida que a String e StringBuffer.
         StringBuilder sb = new StringBuilder("testando");
 
         sb.append(" StringBuilder");
---
+++ src/JavaCore/Colecoes/equals/EqualsTest.java
@@ -0,0 +1,27 @@
+package JavaCore.Colecoes.equals;
+
+import JavaCore.Colecoes.equals.exemple.Smartphone;
+
+public class EqualsTest {
+    public static void main(String[] args) {
+        // O equals() é o método que define o que significa dois objetos serem iguais para a sua aplicação.
+        // equals() ≠ (==).
+        // O operador de igualdade(==) ele apenas compara a referencia de valores em mémoria.
+        // O equals() ele compara o conteúdo dos objetos.
+
+        /* O que acontece se não sobrescrever o equals?
+        * Toda comparação com equals() irá verificar, primeiro, se existe uma sobrescrição do mesmo em ambas as classes. Caso não haja em alguma delas,
+        * o método padrão da classe Object será utilizado. E, no nosso caso, pode ser que os laptops sejam considerados iguais.
+        * */
+
+        Smartphone s1 = new Smartphone("4ABS", "Motorola");
+        Smartphone s2 = new Smartphone("4ABS", "Motorola");
+
+        System.out.println(s1.equals(s2));
+
+
+        // -- Fontes --
+        // DevDojo: https://youtu.be/YWtGWgTlPwQ?si=wK8H-HtOX5Fyi22e
+        // DEVMEDIA: https://www.devmedia.com.br/sobrescrevendo-o-metodo-equals-em-java/26484?utm_dev=google_ads_pmax&gad_source=1&gad_campaignid=22326280955&gclid=Cj0KCQiA2bTNBhDjARIsAK89wlFp7mwItZ-5lZ487fTPGKeKdLKdmzb3KJ70OCwS578M3upqblYnv1AaAoquEALw_wcB
+    }
+}
